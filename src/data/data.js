f953a483c86c707d23b5c6ba4fc960a0d2971487
Surajsingh Gahlod <111622844+surajsinghgahlod@users.noreply.github.com>
2024-02-12 18:33:20 +0530

    first commit

---
+++ src/data/data.js
@@ -0,0 +1,51 @@
+import Loafers from "../images/loafers.jpg";
+import Boots from "../images/boots.png";
+import SportShoes from "../images/spoarts.jpg";
+import Snikkers from "../images/snikkers.webp";
+import Sandals from "../images/sandals.webp";
+import HighHeels from "../images/highheels.webp";
+
+export const MenuList = [
+  {
+    name: "Loafers",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
+    image: Loafers,
+    price: 200,
+  },
+  {
+    name: "Boots",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
+    image: Boots,
+    price: 250,
+  },
+  {
+    name: "Sport Shoes",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
+    image: SportShoes,
+    price: 300,
+  },
+  {
+    name: "Snikkers",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
+    image: Snikkers,
+    price: 100,
+  },
+  {
+    name: "Sandals",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
+    image: Sandals,
+    price: 400,
+  },
+  {
+    name: "High heels",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
+    image: HighHeels,
+    price: 500,
+  },
+];
